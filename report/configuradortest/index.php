707a16295a0d59fd45d457345481f879680a23a6
Ger Mir <gerpollo2000@gmail.com>
2023-10-31 02:38:09 -0600

    Activar Menu: Config Test

--- report/configuradortest/index.php
+++ report/configuradortest/index.php
@@ -22,12 +22,12 @@ $context = context_course::instance($course->id);
 $PAGE->set_title("Configurador de Test");
 $PAGE->set_heading(format_string($course->fullname, true, array('context' => $context)), true);
 
-print_r("------- ");
-print_r($PAGE->navigation);
+/*print_r("------- ");
+print_r($PAGE->navigation);*/
 
-/*print_r("-------2 ");
+print_r("-------2 ");
 print_r($PAGE->navbar);
-
+/*
 print_r("-------3 ");
 print_r($PAGE->secondarynav);
 
