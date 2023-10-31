5ed0ffb3fed76a248dcb3f6835e0d6380d7804ec
Ger Mir <gerpollo2000@gmail.com>
2023-10-31 03:19:15 -0600

    Activar Menu: Config Test

--- report/configuradortest/index.php
+++ report/configuradortest/index.php
@@ -22,27 +22,19 @@ $context = context_course::instance($course->id);
 $PAGE->set_title("Configurador de Test");
 $PAGE->set_heading(format_string($course->fullname, true, array('context' => $context)), true);
 
-/*print_r("------- ");
-print_r($PAGE->navigation);*/
-
-/*print_r("-------2 ");
-print_r($PAGE->navbar);*/
-
-/*print_r("-------3 ");
-print_r($PAGE->secondarynav);*/
-
-/*print_r("-------4 ");
-print_r($PAGE->primarynav);
-*/
-
-/*print_r("-------5 ");
-print_r($PAGE->settingsnav);*/
-
-/*print_r("-------6 ");
-print_r($PAGE->primarynavcombined);*/
-
-
-
 echo $OUTPUT->header();
 
 echo $OUTPUT->footer();
+
+echo "<script>
+    var menus = document.getElementsByClassName('nav-link');
+    var miElemento = null;
+    
+    for (var i = 0; i < menus.length; i++) {
+      if (menus[i].ariaLabel == 'Configurador de Test') {
+          miElemento = menus[i];
+          miElemento.classList.add("active")
+          break;
+      }
+    }
+</script>";
